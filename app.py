a16cff33d9ee33e5203853f93b9d3c475a5d4228
mahmudulshuvo <shuvo0201@gmail.com>
2022-06-23 15:22:05 +0600

    add response

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ import tasks
 queue = Queue(connection=Redis())
 
 def queue_tasks():
-    queue.enqueue(tasks.print_task, 5)
-    queue.enqueue_in(timedelta(seconds=10), tasks.print_numbers, 5)
+    # queue.enqueue(tasks.print_task, 5)
+    # queue.enqueue_in(timedelta(seconds=10), tasks.print_numbers, 5)
+    queue.enqueue(tasks.print_response)
 
 def main():
     queue_tasks()
--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 import time
+import json
+import requests
 
 def print_task(seconds):
     print("Starting task")
@@ -14,3 +16,14 @@ def print_numbers(seconds):
         print(num)
         time.sleep(1)
     print("Task to print_numbers completed")
+
+def print_response():
+    f = open('response.json')
+    data = json.load(f)
+    for item in data:
+        print(item)
+        x = requests.post(f'http://localhost:9200/optimize/_doc/{item["id"]}', json = item)
+        time.sleep(1)
+        print(x)
+        print('\n')
+    f.close()
